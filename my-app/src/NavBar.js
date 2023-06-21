73e4a0180ba1c16264b4095ca53638ba0ea505bb
Tre'Shaun Cottman <Trecottman@gmail.com>
2023-06-21 00:40:51 -0400

    Im going leave home through until we delete it

--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -3,7 +3,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 export default function Navbar() {
   return (
       <nav className="nav">
-        <Link to="/" className="site-title">
+        <Link to="/Home" className="site-title">
         Towson CyberSecurity
         </Link>
         <ul>
