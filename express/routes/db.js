653e74ccdaadf17c0644d3ecb68e9f8dd05ac272
KingAress <99624856+KingAress@users.noreply.github.com>
2023-09-07 19:19:41 -0400

    Updated env and db

--- express/routes/db.js
+++ express/routes/db.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://trecottman60:KingZeus@towsonclub.ja3qb7f.mongodb.net/?retryWrites=true&w=majority";
+require('dotenv').config();
+const uri = process.env.DB_URL;
 const dbName = process.env.DB_NAME;
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 const db = client.db(dbName);
