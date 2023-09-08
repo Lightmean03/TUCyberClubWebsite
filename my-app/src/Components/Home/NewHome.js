f9ee9bdfb0ea54753976a34a6f719afaa2612d6c
Andreas <me@apap04.com>
2023-09-07 20:26:21 -0400

    ? format?
    Signed-off-by: Andreas <me@apap04.com>

--- my-app/src/Components/Home/NewHome.js
+++ my-app/src/Components/Home/NewHome.js
@@ -4,10 +4,14 @@ export default function NewHome() {
   return (
     <div className="w-full border-b">
       <div className="flex mx-6 px-6 pt-12 md:mx-26 md:px-16 xl:px-24 xl:mx-44 items-center justify-between">
-        <h1 className="w-56 md:w-auto text-xl md:text-2xl lg:text-4xl xl:text-5xl ">
+        <h1 className="w-56 md:w-auto text-xl md:text-2xl lg:text-4xl xl:text-5xl bg-gradient-to-r text-transparent from-[#FFD700] to-white bg-clip-text">
           Towson Cyber Defense Club
         </h1>
-        <img src={Spaceman} alt="Welcome" className="w-16 h-16 md:w-72 md:h-64" />
+        <img
+          src={Spaceman}
+          alt="Welcome"
+          className="w-16 h-16 md:w-72 md:h-64"
+        />
       </div>
     </div>
   );
