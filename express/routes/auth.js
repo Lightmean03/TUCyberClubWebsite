4db0c25bfe4a02922e2787ff44a569790605c894
Lightmean03 <kpirmohamedstem2022@gmail.com>
2023-12-27 00:25:46 -0500

    added stuff to db

--- express/routes/auth.js
+++ express/routes/auth.js
@@ -1,58 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const { db } = require('./db');
-const { ObjectId } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { db } = require("./db");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 const saltRounds = 10;
 const secretKey = process.env.JWT_SECRET_KEY;
 const verifyTokenAndRole = require("../controllers/VerifyTokenAndRoles");
 
-router.post('/signup', async (req, res) => {
-  const { email, password } = req.body;
+router.post("/signup", async (req, res) => {
+  const { username, firstname, lastname, email, password } = req.body;
 
   try {
-    const existingUser = await db.collection('users').findOne({ email });
+    const existingUser = await db.collection("users").findOne({ email });
     if (existingUser) {
-      return res.status(409).json({ error: 'User already exists' });
+      return res.status(409).json({ error: "User already exists" });
     }
 
-    if (!email || !password) {
-      return res.status(400).json({ error: 'Email and password must not be empty' });
+    if (!email || !password || !username || !firstname || !lastname) {
+      return res.status(400).json({ error: "Empty Fields " });
     }
 
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     // Set the role to "user" by default
-    const newUser = { email, password: hashedPassword, role: "user", userId: new ObjectId };
-    await db.collection('users').insertOne(newUser);
+    const newUser = {
+      username,
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+      role: "user",
+      userId: new ObjectId(),
+    };
+    await db.collection("users").insertOne(newUser);
 
-    res.json({ message: 'User created successfully' });
+    res.json({ message: "User created successfully" });
   } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
-  console.log("Email: " + email)
+  console.log("Email: " + email);
   try {
-    const user = await db.collection("users").findOne({ email: email});
+    const user = await db.collection("users").findOne({ email: email });
     console.log("Password: " + password);
-    console.log('User password in the database:', user.password);
+    console.log("User password in the database:", user.password);
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     // Compare the password using bcrypt
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
-      return res.status(401).json({ error: 'Incorrect Password' });
+      return res.status(401).json({ error: "Incorrect Password" });
     }
 
     // Build JWT payload
@@ -61,154 +68,172 @@ router.post('/signin', async (req, res) => {
     };
 
     // Generate JWT access token
-    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
-    console.log('Access token:', token);
+    const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+    console.log("Access token:", token);
 
     //store JWT refresh token
-    const refreshToken = jwt.sign({ UserId: user._id }, secretKey, { expiresIn: '1hr' });
-    console.log('Refresh token:', refreshToken);
+    const refreshToken = jwt.sign({ UserId: user._id }, secretKey, {
+      expiresIn: "1hr",
+    });
+    console.log("Refresh token:", refreshToken);
 
     // Store refresh token in database
-    await db.collection("users").updateOne({ email: email }, { $set: { refreshToken: refreshToken } });
+    await db
+      .collection("users")
+      .updateOne({ email: email }, { $set: { refreshToken: refreshToken } });
 
     // Set cookies for tokens and user data
-    res.cookie('token', token, {
-      httpOnly: true, secure: false, sameSite: 'strict', maxAge: 3600000, path: '/',
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 3600000,
+      path: "/",
     });
 
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true, secure: false, sameSite: 'strict', maxAge: 3600000, path: '/',
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 3600000,
+      path: "/",
     });
 
-    res.cookie('user', JSON.stringify(payload), {
-      httpOnly: true, secure: false, sameSite: 'strict', maxAge: 3600000, path: '/',
+    res.cookie("user", JSON.stringify(payload), {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 3600000,
+      path: "/",
     });
 
-    res.status(200).json({ token: token, user: payload, message: 'User signed in successfully' });
+    res.status(200).json({
+      token: token,
+      user: payload,
+      message: "User signed in successfully",
+    });
     console.log("User: " + payload.UserId);
   } catch (error) {
-    console.error('Error signing in:', error);
-    res.status(500).json({ error: 'Failed to sign in' });
+    console.error("Error signing in:", error);
+    res.status(500).json({ error: "Failed to sign in" });
   }
 });
 
-
-
-router.post('/refresh', async (req, res) => {
+router.post("/refresh", async (req, res) => {
   const { refreshToken } = req.body;
 
   try {
     // Verify the refresh token
-    const user = await db.collection('users').findOne({ refreshToken: refreshToken });
+    const user = await db
+      .collection("users")
+      .findOne({ refreshToken: refreshToken });
 
     if (!user) {
-      return res.status(401).json({ message: 'Invalid refresh token' });
+      return res.status(401).json({ message: "Invalid refresh token" });
     }
 
     // Generate a new access token
     const newAccessToken = jwt.sign(
       { email: user.email, role: user.role },
       secretKey,
-      { expiresIn: '1h' }
+      { expiresIn: "1h" }
     );
 
     res.status(200).json({ accessToken: newAccessToken });
   } catch (error) {
-    console.error('Error refreshing token:', error);
-    res.status(500).json({ message: 'Token refresh failed' });
+    console.error("Error refreshing token:", error);
+    res.status(500).json({ message: "Token refresh failed" });
   }
 });
 
-
-
-router.get('/admin', async (req, res) => {
+router.get("/admin", async (req, res) => {
   try {
     const token = req.cookies.token;
-    console.log('Token:', token);
+    console.log("Token:", token);
     if (!token) {
-      return res.status(401).json({ error: 'Unauthorized - Token missing' });
+      return res.status(401).json({ error: "Unauthorized - Token missing" });
     }
 
     const decoded = jwt.verify(token, secretKey);
-    const user = await db.collection('users').findOne({ email: decoded.email });
+    const user = await db.collection("users").findOne({ email: decoded.email });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
-    console.log('User Role:', user.role);
+    console.log("User Role:", user.role);
 
-    if (user.role === 'admin') {
-      return res.status(200).json({ message: 'Access Given' });
+    if (user.role === "admin") {
+      return res.status(200).json({ message: "Access Given" });
     } else {
-      return res.status(403).json({ error: 'Access Forbidden' });
+      return res.status(403).json({ error: "Access Forbidden" });
     }
   } catch (error) {
-    console.error('Error decoding token:', error);
-    return res.status(401).json({ error: 'Unauthorized - Invalid token' });
+    console.error("Error decoding token:", error);
+    return res.status(401).json({ error: "Unauthorized - Invalid token" });
   }
 });
 
-router.get('/users', async (req, res) => {
+router.get("/users", async (req, res) => {
   try {
-    const users = await db.collection('users').find({}, { projection: { email: 1, role: 1 } }).toArray();
+    const users = await db
+      .collection("users")
+      .find({}, { projection: { email: 1, role: 1 } })
+      .toArray();
     res.json(users);
   } catch (error) {
-    console.error('Error getting users:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error getting users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.get('/user', async (req, res) => {
+router.get("/user", async (req, res) => {
   try {
     const token = req.cookies.token;
-    console.log('Token:', token);
+    console.log("Token:", token);
     if (!token) {
-      return res.status(401).json({ error: 'Unauthorized - Token missing' });
+      return res.status(401).json({ error: "Unauthorized - Token missing" });
     }
 
     const decoded = jwt.verify(token, secretKey);
-    const user = await db.collection('users').findOne({ email: decoded.email });
+    const user = await db.collection("users").findOne({ email: decoded.email });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
-    console.log('User Role:', user.role);
+    console.log("User Role:", user.role);
 
-    if (user.role === 'user') {
-      return res.status(200).json({ message: 'Access Given' });
+    if (user.role === "user") {
+      return res.status(200).json({ message: "Access Given" });
     } else {
-      return res.status(403).json({ error: 'Access Forbidden' });
+      return res.status(403).json({ error: "Access Forbidden" });
     }
   } catch (error) {
-    console.error('Error decoding token:', error);
-    return res.status(401).json({ error: 'Unauthorized - Invalid token' });
+    console.error("Error decoding token:", error);
+    return res.status(401).json({ error: "Unauthorized - Invalid token" });
   }
 });
 
-
-
-
-router.get('/users/:role', async (req, res) => {
+router.get("/users/:role", async (req, res) => {
   try {
-    const users = await db.collection('users').find({role: req.params.role}).toArray();
+    const users = await db
+      .collection("users")
+      .find({ role: req.params.role })
+      .toArray();
     res.json(users);
   } catch (error) {
-    console.error('Error getting users:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error getting users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+router.post("/signout", async (req, res) => {
+  res.clearCookie("token");
+  res.clearCookie("user");
+  res.clearCookie("refreshToken");
 
-
-router.post('/signout', async (req, res) => {
-  res.clearCookie('token');
-  res.clearCookie('user');
-  res.clearCookie('refreshToken');
-
-  res.status(200).json({ message: 'User signed out successfully' });
+  res.status(200).json({ message: "User signed out successfully" });
 });
-
 
 module.exports = router;
--- my-app/src/Components/Signup/Signup.js
+++ my-app/src/Components/Signup/Signup.js
@@ -1,28 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Signup.css';
-import { isEmail } from 'validator';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Signup.css";
+import { isEmail } from "validator";
+import { useNavigate } from "react-router-dom";
 
 const Signup = ({ showSignup, setShowSignup }) => {
   const [validated, setValidated] = useState(false);
   const [form, setForm] = useState({
-   email: '',
-   password: '',
+    username: "",
+    firstname: " ",
+    lastname: " ",
+    email: "",
+    password: "",
   });
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
   const MIN_PASSWORD_LENGTH = 8;
 
-  // Error messages 
+  // Error messages
   const errorMessages = {
+    username: {
+      required: "Please provide a username.",
+    },
+    firstname: {
+      required: "Please provide a first name.",
+    },
     email: {
-      required: 'Please provide an email address.',
-      invalid: 'Please provide a valid email address.',
+      required: "Please provide an email address.",
+      invalid: "Please provide a valid email address.",
     },
     password: {
-      required: 'Please provide a password.',
-      minLength: 'Password must be at least 8 characters long.',
+      required: "Please provide a password.",
+      minLength: "Password must be at least 8 characters long.",
     },
   };
 
@@ -31,15 +40,15 @@ const Signup = ({ showSignup, setShowSignup }) => {
     let newErrors = {};
 
     if (!form.email) {
-      newErrors.email = 'required';
+      newErrors.email = "required";
     } else if (!isEmail(form.email)) {
-      newErrors.email = 'invalid';
+      newErrors.email = "invalid";
     }
 
     if (!form.password) {
-      newErrors.password = 'required';
+      newErrors.password = "required";
     } else if (form.password.length < MIN_PASSWORD_LENGTH) {
-      newErrors.password = 'minLength';
+      newErrors.password = "minLength";
     }
 
     setErrors(newErrors);
@@ -49,13 +58,13 @@ const Signup = ({ showSignup, setShowSignup }) => {
   // Sign-up function
   const handleSignUp = () => {
     axios
-      .post('http://localhost:9000/auth/Signup', form)
+      .post("http://localhost:9000/auth/Signup", form)
       .then((response) => {
-        console.log('Signup response:', response.data);
-        navigate('/signin');
+        console.log("Signup response:", response.data);
+        navigate("/signin");
       })
       .catch((error) => {
-        console.error('Error signing up:', error);
+        console.error("Error signing up:", error);
       });
   };
 
@@ -71,8 +80,11 @@ const Signup = ({ showSignup, setShowSignup }) => {
   // Form reset function
   const handleReset = () => {
     setForm({
-     email: '',
-   password: '',
+      username: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
     });
     setValidated(false);
     setErrors({});
@@ -85,93 +97,112 @@ const Signup = ({ showSignup, setShowSignup }) => {
     setErrors((prevErrors) => ({ ...prevErrors, [name]: undefined }));
   };
 
- 
   return (
-    <div className='bg2'>
-
-    <div className="modal">
-      <div className="modal-dialog" onClick={(e) => e.stopPropagation()}>
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title signup-title">SIGN UP</h5>
-          </div>
-          <div className="modal-body">
-            <div className="signup-container">
-              <div className="row">
-                <div className="col">
-                  <div className="form-group mb-3">
-                    <label htmlFor="email" className="email">
-                      Email Address:
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      value={form.email}
-                      onChange={handleChange}
-                      required
-                      className={`form-control ${
-                        validated && errors.email ? 'is-invalid' : ''
-                      }`}
-                    />
-                    {validated && errors.email && (
-                      <div className="invalid-feedback">
-                        {errorMessages.email[errors.email]}
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <div className="col">
-                  <div className="form-group mb-5">
-                    <label htmlFor="password" className="password">
-                      Password:
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      onChange={handleChange}
-                      value={form.password}
-                      required
-                      className={`form-control ${
-                        validated && errors.password ? 'is-invalid' : ''
-                      }`}
-                    />
-                    {validated && errors.password && (
-                      <div className="invalid-feedback">
-                        {errorMessages.password[errors.password]}
-                      </div>
-                    )}
-                    <small className="form-text text-muted">
-                      Password must be at least 8 characters long.
-                    </small>
-                  </div>
-                </div>
-              </div>
-              <button
-                type="button"
-                className="btn btn-primary signup-submit-btn"
-                onClick={handleSubmit}
-              >
-                Submit
-              </button>
-              <button
-                type="button"
-                className="btn btn-secondary signup-reset-btn"
-                onClick={handleReset}
-              >
-                Reset
-              </button>
-              <button
-              >
-                <a href="/signin" className="signup-close-btn flex justify-start items-start text-left text-1.8rem mt-2rem text-white">x</a>
-              </button>
-
+    <div className="bg2">
+      <div className="modal">
+        <div className="modal-dialog" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title signup-title">SIGN UP</h5>
             </div>
-            
+            <div className="modal-body">
+              <div className="signup-container">
+                <div className="row">
+                  <div className="col">
+                    <div className="form-group mb-3">
+                      <label htmlFor="username" className="username">
+                        Username:
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={form.email}
+                        onChange={handleChange}
+                        required
+                        className={`form-control ${
+                          validated && errors.email ? "is-invalid" : ""
+                        }`}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col">
+                    <div className="form-group mb-3">
+                      <label htmlFor="email" className="email">
+                        Email Address:
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={form.email}
+                        onChange={handleChange}
+                        required
+                        className={`form-control ${
+                          validated && errors.email ? "is-invalid" : ""
+                        }`}
+                      />
+                      {validated && errors.email && (
+                        <div className="invalid-feedback">
+                          {errorMessages.email[errors.email]}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="form-group mb-5">
+                      <label htmlFor="password" className="password">
+                        Password:
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        value={form.password}
+                        required
+                        className={`form-control ${
+                          validated && errors.password ? "is-invalid" : ""
+                        }`}
+                      />
+                      {validated && errors.password && (
+                        <div className="invalid-feedback">
+                          {errorMessages.password[errors.password]}
+                        </div>
+                      )}
+                      <small className="form-text text-muted">
+                        Password must be at least 8 characters long.
+                      </small>
+                    </div>
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-primary signup-submit-btn"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary signup-reset-btn"
+                  onClick={handleReset}
+                >
+                  Reset
+                </button>
+                <button>
+                  <a
+                    href="/signin"
+                    className="signup-close-btn flex justify-start items-start text-left text-1.8rem mt-2rem text-white"
+                  >
+                    x
+                  </a>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
     </div>
   );
 };
