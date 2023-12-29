c1a9d42afd5de4520c3b4fb1a771b334cd20eab0
Andreas <git@apap04.com>
2023-12-28 19:17:16 -0500

    add shadcn
    Signed-off-by: Andreas <git@apap04.com>

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -7,5 +8,10 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
