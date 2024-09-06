59cf646981fb6172c4d1d150199d01f0808d67aa
KingAress <99624856+KingAress@users.noreply.github.com>
2024-09-05 22:55:39 -0400

    db update

--- backend/backend/authentication/api/views/views.py
+++ backend/backend/authentication/api/views/views.py
@@ -62,8 +62,7 @@ def signout(request):
     except Exception as e:
         print(f"Unexpected error in signout: {str(e)}")
         return Response({'error': str(e)}, status=400)
-
-
+    
     
 
 @api_view(['GET'])
