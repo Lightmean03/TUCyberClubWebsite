faec2819e294d98dcecc2a67a84e2dea21044247
KingAress <99624856+KingAress@users.noreply.github.com>
2024-09-07 23:42:16 -0400

    Signin ui fixes.

--- my-app/src/Components/Signup/Signup.tsx
+++ my-app/src/Components/Signup/Signup.tsx
@@ -171,7 +171,7 @@ const Signup: React.FC = () => {
             <div className="mt-4">
               <Link
                 to="/signin"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black bg-white hover:bg-gray-50 text-blue-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium  bg-white hover:bg-gray-50 text-blue-500"
               >
                 Sign in
               </Link>
